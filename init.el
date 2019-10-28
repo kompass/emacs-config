68d9d93d483944039ff043090b0afea70e1e7a10
Nicolas Kirchner <nicolas.kirchner@orange.com>
2019-10-28 20:49:06 +0100

    First config from .emacs to init.el

---
+++ init.el
@@ -0,0 +1,111 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("gnu" . "http://mirrors.163.com/elpa/gnu/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (eval-when-compile (require 'use-package)))
+
+(setq use-package-always-ensure t)
+
+(use-package benchmark-init
+  :config
+  ;; To disable collection of benchmark data after init is done.
+  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
+(add-hook 'after-init-hook
+          (lambda () (message "loaded in %s" (emacs-init-time))))
+
+(setq gc-cons-threshold 10000000)
+
+;; Restore after startup
+(add-hook 'after-init-hook
+          (lambda ()
+            (setq gc-cons-threshold 1000000)
+            (message "gc-cons-threshold restored to %S"
+                     gc-cons-threshold)))
+
+(setq custom-file (make-temp-file "emacs-custom"))
+
+(setq inhibit-startup-screen t)
+
+(global-visual-line-mode 1)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+
+(setq ring-bell-function 'ignore)
+
+;;(use-package panda-theme
+;;  :ensure t
+;;  :config
+;;  (load-theme 'panda t))
+
+(use-package gruvbox-theme
+  :ensure t
+  :config
+  (load-theme 'gruvbox-dark-medium t))
+
+(set-frame-font "Fira Code 10" nil t)
+
+(use-package emojify)
+
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+(add-to-list 'load-path "~/.emacs.d/undo-tree")
+(add-to-list 'load-path "~/.emacs.d/goto-chg")
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
+(setq evil-emacs-state-cursor '("red" box))
+(setq evil-normal-state-cursor '("green" box))
+(setq evil-visual-state-cursor '("orange" box))
+(setq evil-insert-state-cursor '("red" bar))
+(setq evil-replace-state-cursor '("red" bar))
+(setq evil-operator-state-cursor '("red" hollow))
+
+(add-to-list 'load-path "~/.emacs.d/evil-leader")
+(require 'evil-leader)
+(setq evil-leader/in-all-states 1)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+
+(setq-default display-line-numbers-type 'relative)
+(setq-default display-line-numbers-widen t)
+(custom-set-faces '(line-number-current-line ((t :weight bold
+                                                 :foreground "goldenrod"))))
+(global-display-line-numbers-mode)
+
+(setq scroll-margin 10
+      scroll-conservatively 9999
+      scroll-step 1)
+
+;; esc quits
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit. In Delete Selection mode, if the mark is active, just deactivate it; then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark  t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+(global-set-key [escape] 'evil-exit-emacs-state)
+
+;; start maximized
+(custom-set-variables '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+;; use spaces instead of tabs
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
